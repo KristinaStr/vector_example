74f3ce9fdcf155e058d416842847036664bf54a6
KristinaStr <32191518+KristinaStr@users.noreply.github.com>
2018-05-06 22:23:56 +0300

    Update vector.cpp

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -5,24 +5,24 @@
 
 TEST_CASE("creating vector")
 {
-	vector_t vector;
+	vector_t<int> vector;
 	REQUIRE( vector.size() == 0 );
 	REQUIRE( vector.capacity() == 0 );
 }
 
 TEST_CASE("copying vector")
 {
-	vector_t vector;
-	vector.push_back(1);
+	vector_t<double> vector;
+	vector.push_back(0.33);
 
-	vector_t copy(vector);
+	vector_t<double> copy(vector);
 	REQUIRE( copy == vector );
 }
 
 TEST_CASE("assigning vector")
 {
-	vector_t vector1;
-	vector_t vector2;
+	vector_t<int> vector1;
+	vector_t<int> vector2;
 
 	vector1.push_back(1);
 	vector2.push_back(2);
@@ -33,8 +33,8 @@ TEST_CASE("assigning vector")
 
 TEST_CASE("equaling vector")
 {
-	vector_t vector1;
-	vector_t vector2;
+	vector_t<int> vector1;
+	vector_t<int> vector2;
 
 	vector1.push_back(1);
 	vector2.push_back(1);
@@ -47,19 +47,19 @@ TEST_CASE("equaling vector")
 
 TEST_CASE("indexing vector")
 {
-	vector_t vector;
+	vector_t<int> vector;
 
 	vector.push_back(1);
 
 	REQUIRE( vector[0] == 1 );
 
-	vector_t const copy(vector);
+	vector_t<int> const copy(vector);
 	REQUIRE( copy[0] == 1 );
 }
 
 TEST_CASE("pushing elements")
 {
-	vector_t vector;
+	vector_t<int> vector;
 
 	vector.push_back(1);
 	REQUIRE( vector.size() == 1 );
@@ -84,7 +84,7 @@ TEST_CASE("pushing elements")
 
 TEST_CASE("poping elements")
 {
-	vector_t vector;
+	vector_t<int> vector;
 
 	vector.push_back(1);
 	vector.push_back(2);
@@ -116,4 +116,22 @@ TEST_CASE("poping elements")
 	vector.pop_back();
 	REQUIRE( vector.size() == 0 );
 	REQUIRE( vector.capacity() == 1 );
+}
+TEST_CASE("Range_good")
+{
+	vector_t<int> vector ;
+	vector.push_back(2);
+	REQUIRE_NOTHROW ( vector.at(0));
+}
+TEST_CASE("Range_bad")
+{
+	vector_t<int> vector ;
+	vector.push_back(2);
+	REQUIRE_THROWS_AS( vector.at(1) , std::out_of_range);
+}
+TEST_CASE("Other_type")
+{
+	vector_t<double> vector ;
+	vector.push_back(2.1);
+	REQUIRE( vector.at(0) == 2.1);
 }
